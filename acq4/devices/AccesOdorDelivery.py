484a836721944ea3b634389ebe8e828e3c708f47
Martin Chase <outofculture@gmail.com>
2022-07-12 01:07:21 -0500

    structure for OdorDelivery that better matches conception
    new dev gui with boxes for each channel

--- acq4/devices/AccesOdorDelivery.py
+++ acq4/devices/AccesOdorDelivery.py
@@ -3,14 +3,19 @@ from acq4.drivers.Acces.UsbDIO96 import UsbDIO96, DEFAULT_SINGLE_DEVICE_ID
 
 
 class AccesOdorDelivery(OdorDelivery):
+    _port_states: "dict[int, dict[int, bool]]"  # {channel: {port: enabled, ...}, ...}
+
     def __init__(self, deviceManager, config: dict, name: str):
         super().__init__(deviceManager, config, name)
 
         self._dev = UsbDIO96(config.get("deviceId", DEFAULT_SINGLE_DEVICE_ID))
         output_ports = {ch for group in self.odors.values() for ch in group}
         self._dev.configure_ports(UsbDIO96.OUTPUT, output_ports)
+        self._port_states = {}
         self._triggerReadChannel = config.get("triggerReadChannel", 11)
         self._daqTriggerChannel = config.get("daqTriggerChannel")  # TODO default? format?
 
-    def setChannelValue(self, channel: int, value: int):
-        self._dev.write(channel, value)
+    def setChannelPortEnabled(self, channel: int, port: int, enabled: bool):
+        # TODO special handling of port 1?
+        self._port_states.setdefault(channel, {})[port] = enabled
+        self._dev.write(channel, sum(self._port_states[channel].values()))
--- acq4/devices/OdorDelivery.py
+++ acq4/devices/OdorDelivery.py
@@ -1,39 +1,36 @@
 import threading
+from typing import Union
 
-from pyqtgraph import SpinBox
 from .Device import Device, TaskGui, DeviceTask
 from ..util import Qt
 from ..util.future import Future
 
 
 class OdorDelivery(Device):
-    odors: dict[str, dict[int: str]]  # {group_name: {channel: odor_name, ...}, ...}
+    # {channel_name: {channel: number, ports: {port_number: odor_name, ...}}, ...}
+    odors: "dict[str, dict[str : Union[int, dict[int:str]]]]"
 
     def __init__(self, deviceManager, config: dict, name: str):
         super().__init__(deviceManager, config, name)
         self.odors = {
-            group: {int(ch): name for ch, name in values.items()}
-            for group, values in config.get("odors", {}).items()
+            group: {
+                "channel": int(group_config["channel"]),
+                "ports": {int(port): name for port, name in group_config["ports"].items()},
+            }
+            for group, group_config in config.get("odors", {}).items()
         }
-        self.activeOdorGroup = next(iter(self.odors), None)
-        # TODO can/should we read channel state?
 
     def odorChannels(self):
-        return self.odors.get(self.activeOdorGroup, {})
+        return sorted([gr["channel"] for gr in self.odors.values()])
 
-    def setActiveOdorGroup(self, newGroup):
-        if newGroup not in self.odors:
-            raise ValueError(f"{newGroup} is not a valid odor group name")
-        self.activeOdorGroup = newGroup
-
-    def setChannelValue(self, channel: int, value: int):
-        """Turn a given odor channel on or off"""
+    def setChannelPortEnabled(self, channel: int, port: int, enabled: bool):
+        """Turn a given odor channel/port on or off"""
         raise NotImplementedError()
 
     def setAllChannelsOff(self):
         """Turn off all odors. (Reimplement if that should be handled other than by iterating)"""
         for ch in self.odorChannels():
-            self.setChannelValue(ch, 0)
+            self.setChannelPortEnabled(ch, 1, False)
 
     def deviceInterface(self, win):
         return OdorDevGui(self)
@@ -41,6 +38,9 @@ class OdorDelivery(Device):
     def taskInterface(self, task):
         return None
         # return OdorTaskGui(self, task)
+
+    def createTask(self, cmd, parentTask):
+        return OdorTask(self, cmd, parentTask)
 
 
 class OdorDevGui(Qt.QWidget):
@@ -56,55 +56,40 @@ class OdorDevGui(Qt.QWidget):
     def __init__(self, dev: OdorDelivery):
         super().__init__()
         self.dev = dev
-        self.layout = Qt.QGridLayout()
+        self.layout = Qt.FlowLayout()
         self.setLayout(self.layout)
-        self._groupSelector = Qt.QComboBox()
-        for group in dev.odors:
-            self._groupSelector.addItem(group)
-        self._intensitySpin = SpinBox(value=1, int=True, step=1, bounds=(0, 2 ** 8 - 1), compactHeight=False)
-        self._intensitySpin.valueChanged.connect(self._handleIntensityChange)
-        self.layout.addWidget(self._intensitySpin, 0, 0)
-        self._groupSelector.setCurrentText(dev.activeOdorGroup)
-        self.layout.addWidget(self._groupSelector, 0, 1)
-        self._groupSelector.currentTextChanged.connect(self._handleGroupChange)
-        self._odorLayout = Qt.FlowLayout()
-        self._odorGroup = Qt.QButtonGroup()
-        self.layout.addLayout(self._odorLayout, 1, 0, 1, 2)
+        self._buttonGroups = {}
         self._setupOdorButtons()
 
     def _setupOdorButtons(self):
-        odors = self.dev.odorChannels()
-        off_button = Qt.QRadioButton(self.OFF_LABEL)
-        off_button.setObjectName(self.OFF_LABEL)
-        off_button.setChecked(True)
-        self._odorGroup.addButton(off_button)
-        self._odorLayout.addWidget(off_button)
-        off_button.toggled.connect(self._handleOdorToggle)
-        for channel, odor in odors.items():
-            button = Qt.QRadioButton(f"{channel}: {odor}")
-            button.setObjectName(str(channel))
-            self._odorGroup.addButton(button)
-            self._odorLayout.addWidget(button)
-            button.toggled.connect(self._handleOdorToggle)
+        for group_name, group_config in self.dev.odors.items():
+            channel = group_config["channel"]
+            group_box = Qt.QGroupBox(f"{channel}: {group_name}")
+            group_box.setCheckable(True)
+            group_box.setChecked(False)
+            group_box.setObjectName(group_name)
+            group_box.clicked.connect(self._handleOffButtonPush)
+            group_layout = Qt.FlowLayout()
+            group_box.setLayout(group_layout)
+            self.layout.addWidget(group_box)
 
-    def _handleGroupChange(self, newGroup):
-        self.dev.setAllChannelsOff()
-        self.dev.setActiveOdorGroup(newGroup)
-        for button in self._odorGroup.buttons():
-            self._odorGroup.removeButton(button)
-        self._odorLayout.clear()
-        self._setupOdorButtons()
+            for port, odor in group_config["ports"].items():
+                button = Qt.QCheckBox(f"{channel}[{port}]: {odor}")
+                button.setObjectName(f"{channel}:{port}")
+                group_layout.addWidget(button)
+                button.clicked.connect(self._handleOdorButtonPush)
 
-    def _handleIntensityChange(self, newVal):
-        channel = self._odorGroup.checkedButton().objectName()
-        if channel != self.OFF_LABEL:
-            self.dev.setChannelValue(int(channel), newVal)
-
-    def _handleOdorToggle(self, enabled):
+    def _handleOffButtonPush(self, enabled):
         btn = self.sender()
-        channel = btn.objectName()
-        if channel != self.OFF_LABEL:
-            self.dev.setChannelValue(int(channel), self._intensitySpin.value() if enabled else 0)
+        group_name = btn.objectName()
+        channel = self.dev.odors[group_name]["channel"]
+        self.dev.setChannelPortEnabled(channel, 1, enabled)
+        # TODO maybe also uncheck all buttons?
+
+    def _handleOdorButtonPush(self, enabled):
+        btn = self.sender()
+        channel, port = map(int, btn.objectName().split(":"))
+        self.dev.setChannelPortEnabled(channel, port, enabled)
 
 
 class OdorTaskGui(TaskGui):
@@ -119,9 +104,12 @@ class OdorTaskGui(TaskGui):
         raise "TODO"
 
     def generateTask(self, params=None):
+        # params=None means "single"
+        # otherwise, we'll get a dict with a key: list
         raise "TODO"
 
     def listSequence(self):
+        # for if this task is being combinatorially expanded
         raise "TODO"
 
     # TODO should this also let the user select which group of odors is active? it's not much more.
@@ -130,16 +118,22 @@ class OdorTaskGui(TaskGui):
 
 class OdorTask(DeviceTask):
     def __init__(self, dev, cmd, parentTask):
+        """cmd structure: """
         super().__init__(dev, cmd, parentTask)
         self._cmd = cmd
+        self.dev.setAllChannelsOff()
+        # TODO turn on control bit for all relevant channels
         # TODO set up the DAQ trigger signal
 
     def configure(self):
-        self.dev.setAllChannelsOff()
         # TODO if using a trigger line, we can start the listening thread
+        pass
 
     def isDone(self):
         pass  # todo
+
+    def getResult(self):
+        raise "TODO"  # save the cmd
 
     def start(self):
         pass  # TODO
