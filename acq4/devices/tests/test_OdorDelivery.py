5182b9dd5bb344acd65c006faab24bb57c069ee4
Martin Chase <outofculture@gmail.com>
2022-10-10 16:59:38 -0700

    test of the ListSeqParameter

--- acq4/devices/tests/test_OdorDelivery.py
+++ acq4/devices/tests/test_OdorDelivery.py
@@ -5,7 +5,7 @@ import time
 import unittest
 
 from acq4.devices.MockOdorDelivery import MockOdorDelivery
-from acq4.devices.OdorDelivery import OdorTask
+from acq4.devices.OdorDelivery import OdorTask, _ListSeqParameter
 
 
 class OdorDeliveryTests(unittest.TestCase):
@@ -37,3 +37,15 @@ class OdorDeliveryTests(unittest.TestCase):
         while not task.isDone():
             time.sleep(0.1)
         dev.setChannelValue.assert_has_calls([call(0, 1), call(0, 2), call(0, 6), call(0, 4), call(0, 0)])
+
+
+class _ListSeqParameterTest(unittest.TestCase):
+    def test_sequence_compile(self):
+        p = _ListSeqParameter(
+            name="Test",
+            type="list",
+            limits={"a": 10, "b": 11, "c": 3, "d": 8, "e": 7},
+            group_by={"oddness": lambda name, num: num % 2 > 0}
+        )
+        p.setState({"sequence": "oddness", "oddness": True})
+        self.assertEqual(p["oddness"], [11, 3, 7])
